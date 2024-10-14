43c09621433226a762cf5c864d4b37fe23e199a4
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-10-14 11:45:16 +0500

    Update upgrade.js

--- js/classes/upgrade.js
+++ js/classes/upgrade.js
@@ -1,25 +1,43 @@
-Vue.component("upgrade", {
-	props: ["upgrade"],
-	methods: 
+class Upgrade
+{
+	constructor(name, desc, in_Price, priceIncrease, multi, prefix)
 	{
-		formatNumber: function(n, prec, lim)
-		{
-			return format(n, prec, lim)
-		},
-	},
-	computed:
+    		this.name = name;
+		this.desc = desc;
+		this.in_Price = in_Price;
+		this.priceIncrease = priceIncrease;
+		this.multi = multi; // mor poents
+		this.prefix = "×";
+		this.level = new OmegaNum(0);
+	}
+
+	getMultiplier()
 	{
-		canAfford: function()
+		let base = E(this.multi).pow(this.level);
+		let softcap = base.gte(this.multi.pow(1000)) ? (this.level.add(573).mul(0.00063589192)) : E(1); // Here goes a softcap
+		let softcap2 = softcap.gte(1000) ? (softcap.add(573).mul(0.00063589192)) : E(1); // Softcap^2
+		return base.div(softcap).div(softcap2);
+	}
+
+	getPrice()
+	{
+		let price = this.in_Price.pow(Decimal.pow(this.priceIncrease, this.level));
+		let dilating = price.gte(E(10).pow(E(Number.MAX_VALUE))) ? ((OmegaNum.log(OmegaNum.root(price.pow(E(Number.MAX_VALUE)), 1e135), 10) / 2) + 1) : 1;
+		return price.pow(dilating);
+	}
+	
+	buy() {
+		if (this.getPrice().lt(game.number))
 		{
-			return this.upgrade.getPrice().lt(game.points)
-      		},
-	},
-	template:
-	`
-  		<button :disabled="!canAfford" @click="numberupgrade.buy()">
- 		<nu>{{upgrade.name}}</nu>
- 		<nt>{{upgrade.desc}}</nt>
- 		<nt>Cost: {{formatNumber(upgrade.level, 0, 10)}}</nt>
- 		</button>
- 	`
-})
+			game.number = game.number.div(this.getPrice());
+			this.level = new Decimal(this.level).add(1);
+			return true;
+		}
+		return false;
+	}
+	
+	buyMax()
+	{
+		while (this.buy());
+	}
+}
