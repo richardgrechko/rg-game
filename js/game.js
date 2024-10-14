992f7554e18ae04c20f8bb365753be0d4fdfcee1
Richardson736 <162537734+richardgrechko@users.noreply.github.com>
2024-10-14 20:24:11 +0500

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,7 +1,5 @@
 let game = {
 	points: new ExpantaNum(2),
 	exponent: new ExpantaNum(2),
-	upgrades: [
-		new Upgrade("exponent","your first upgrade",new ExpantaNum(2),new ExpantaNum(2),new ExpantaNum(1.8),"^")
-	],
+	upgrades: [],
 };
